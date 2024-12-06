008b2baabbe1feee6a4685e5336b3ac71dc999e0
Chance <chance.fyii@gmail.com>
2024-12-06 18:21:55 +0800

    Optimize canvas curve drawing

--- src/core/event.ts
+++ src/core/event.ts
@@ -5,22 +5,25 @@ interface Params {
   canvas: CanvasRenderingContext2D
   upCallback: (t: Event) => void
   config: SyncConfigInterface
+  setting: boolean
 }
 
 export class Event {
   public canvas: CanvasRenderingContext2D
   public readonly upCallback: (t: Event) => void
   public config: SyncConfigInterface
+  public setting: boolean
   public left: number
   public top: number
   private lastX: number
   private lastY: number
   private blockMenu: boolean = false
 
-  constructor({ canvas, upCallback, config }: Params) {
+  constructor({ canvas, upCallback, config, setting }: Params) {
     this.canvas = canvas
     this.upCallback = upCallback
     this.config = config
+    this.setting = setting
 
     this.mouseMove = this.mouseMove.bind(this)
     this.mouseUp = this.mouseUp.bind(this)
@@ -35,11 +38,19 @@ export class Event {
   }
 
   public mouseDown(e: MouseEvent) {
-    const rect = (e.target as HTMLCanvasElement).getBoundingClientRect()
-    this.left = rect.left
-    this.top = rect.top
-    this.lastX = e.clientX - rect.left
-    this.lastY = e.clientY - rect.top
+    if (e.button !== 2) {
+      return
+    }
+    if (this.setting) {
+      const rect = (e.target as HTMLElement).getBoundingClientRect()
+      this.left = rect.left
+      this.top = rect.top
+    } else {
+      this.left = 0
+      this.top = 0
+    }
+    this.lastX = e.clientX - this.left
+    this.lastY = e.clientY - this.top
     document.addEventListener("mousemove", this.mouseMove)
     document.addEventListener("mouseup", this.mouseUp)
   }
@@ -47,21 +58,29 @@ export class Event {
   public mouseMove(e: MouseEvent) {
     const currentX: number = e.clientX - this.left
     const currentY: number = e.clientY - this.top
+
+    const distance = Math.sqrt(
+      Math.pow(currentX - this.lastX, 2) + Math.pow(currentY - this.lastY, 2)
+    )
+
     this.canvas.beginPath()
     this.canvas.moveTo(this.lastX, this.lastY)
-    // Using Bessel curves to smooth trajectories
-    this.canvas.quadraticCurveTo(
-      (this.lastX + currentX) / 2,
-      (this.lastY + currentY) / 2,
-      currentX,
-      currentY
-    )
+
+    // Bessel curve
+    const smoothness = Math.min(0.5, distance / 100)
+    const controlX = this.lastX + (currentX - this.lastX) * smoothness
+    const controlY = this.lastY + (currentY - this.lastY) * smoothness
+
+    this.canvas.quadraticCurveTo(controlX, controlY, currentX, currentY)
+
     this.canvas.strokeStyle = this.config.strokeStyle
     this.canvas.lineWidth = this.config.lineWidth
     this.canvas.lineCap = "round"
     this.canvas.lineJoin = "round"
+
     this.canvas.stroke()
     this.canvas.closePath()
+
     this.lastX = currentX
     this.lastY = currentY
 

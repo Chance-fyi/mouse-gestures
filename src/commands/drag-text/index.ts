3f11a159e2b7cc1cc54c7afecf665a740f53755a
Chance <chance.fyii@gmail.com>
2025-02-08 14:06:49 +0800

    Add command to open dragged text link

--- src/commands/drag-text/index.ts
+++ src/commands/drag-text/index.ts
@@ -1,4 +1,5 @@
 import { Copy } from "~commands/drag-text/copy"
+import { Open } from "~commands/drag-text/open"
 import { Search } from "~commands/drag-text/search"
 
-export default [new Copy(), new Search()]
+export default [new Copy(), new Search(), new Open()]

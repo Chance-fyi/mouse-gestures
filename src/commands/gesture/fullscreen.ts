14aed95c210822f2ff75d9e71a6f2ee97c9e1cea
Chance <chance.fyii@gmail.com>
2026-02-27 11:04:12 +0800

    Switch fullscreen gesture to browser-level fullscreen (chrome.windows API) close #33

--- src/commands/gesture/fullscreen.ts
+++ src/commands/gesture/fullscreen.ts
@@ -1,18 +1,21 @@
 import type { CommandInterface } from "~commands/command-interface"
 
+let previousState: chrome.windows.windowStateEnum = "maximized"
+
 export class Fullscreen implements CommandInterface {
   readonly uniqueKey: string = "gesture-fullscreen"
   readonly title: string = "command_gesture_fullscreen_title"
   readonly description: string = "command_gesture_fullscreen_description"
   config: { [key: string]: any } = {}
 
-  window: boolean = true
-
   execute(): void {
-    if (!document.fullscreenElement) {
-      document.documentElement.requestFullscreen().then()
-    } else {
-      document.exitFullscreen().then()
-    }
+    chrome.windows.getCurrent((win) => {
+      if (win.state === "fullscreen") {
+        chrome.windows.update(win.id, { state: previousState }).then()
+      } else {
+        previousState = win.state as chrome.windows.windowStateEnum
+        chrome.windows.update(win.id, { state: "fullscreen" }).then()
+      }
+    })
   }
 }
